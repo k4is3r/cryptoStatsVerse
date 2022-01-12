db7d2f4086c473b8aba20ba61b95bce33eff1ebc
Eduardo Imery <ejwimery@gmail.com>
2022-01-12 09:37:35 -0600

    creating navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar} from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons/lib/icons';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+};
+import { Button, Menu, Typography, Avatar} from 'antd';
+
+
+export default Navbar
