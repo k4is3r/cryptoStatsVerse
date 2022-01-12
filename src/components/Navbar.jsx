c2dea0fe36a3831cd2f7b37f5a40d2768d2d02f4
Eduardo Imery <ejwimery@gmail.com>
2022-01-12 09:55:16 -0600

    starting structure for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,19 @@ import { Button, Menu, Typography, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons/lib/icons';
 
-
+import icon from '../images/logo.png';
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar src={icon} size="large"/>
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Cryptoverse</Link>
+                </Typography.Title>
+                {/* <Button className="menu-control-container">
+
+                </Button> */}
+            </div>
         </div>
     )
 };
