a5f124892daaae61e2abbdc7a35d9954b0ae04ce
Eduardo Imery <ejwimery@gmail.com>
2022-01-15 13:55:56 -0600

    creating first look of Homepage component

--- src/App.js
+++ src/App.js
@@ -33,9 +33,17 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-            </div>
             <div className="footer">
-
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center'}}>
+                    Cryptoverse <br />
+                    All rights reserverd
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News </Link>
+                </Space>
+            </div>
             </div>
         </div>
     )
