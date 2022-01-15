84e56c4daf98a243cf7ae9c45d958ca37d20b0c8
Eduardo Imery <ejwimery@gmail.com>
2022-01-15 12:51:31 -0600

    creating main secction, defining structure components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Swtich, Route, Link} from 'react-router-dom';
+import { Swtich, Route, Link, Routes} from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, CryptoDetails, Cryptocurrencies, News } from './components';
 import './App.css';
 
 const App = () => {
@@ -12,7 +12,27 @@ const App = () => {
                 <Navbar/> 
             </div>
             <div className="main">
-
+                <Layout>
+                    <div className="routes">
+                        <Swtich>
+                            <Route exact path="/">
+                                <Homepage />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/crypto/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Swtich>
+                    </div>
+                </Layout>
             </div>
             <div className="footer">
 
