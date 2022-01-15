e008d608be9d5b7b3bacf3ecb725bde63d24e0ad
Eduardo Imery <ejwimery@gmail.com>
2022-01-15 12:39:58 -0600

    creating avatar, menu and adding styles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Swtich, Route, Link} from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
+import { Navbar } from './components';
+import './App.css';
 
 const App = () => {
     return (
         <div className="app">
             <div className="navbar">
-
+                <Navbar/> 
             </div>
             <div className="main">
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,33 @@ import { Button, Menu, Typography, Avatar} from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons/lib/icons';
 
-import icon from '../images/logo.png';
+import icon from '../images/logo2.png';
 const Navbar = () => {
     return (
         <div className="nav-container">
             <div className="logo-container">
-                <Avatar src={icon} size="large"/>
+                <Avatar src={icon} size="small"/>
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Cryptoverse</Link>
                 </Typography.Title>
-                {/* <Button className="menu-control-container">
-
-                </Button> */}
             </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined/>}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined/>}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
-    )
-};
-import { Button, Menu, Typography, Avatar} from 'antd';
+    );
+}
 
 
 export default Navbar
