8da41922f4df1e80d29f7368a902fd9014b7d48f
Eduardo Imery <ejwimery@gmail.com>
2022-01-15 23:57:14 -0600

    testing fetching and showing total coins

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-    'x-rapidapi-key': process.env.REACT_API_KEY_RAPIDAPI_COINRANKING,
     'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_CRYPTO_HOST,
+    'x-rapidapi-key': process.env.REACT_API_KEY_RAPIDAPI_COINRANKING,
 }
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_RAPIDAPI_CRYPO_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_RAPIDAPI_CRYPTO_URL }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: () => createRequest('/coins')
